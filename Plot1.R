1b8de76d2ef0b30ee21b3b53b41de58cdc612c36
Aditya Laghate <me@adityalaghate.in>
2014-06-07 10:16:00 +0530

    Adding code for plot1 and plot2

---
+++ Plot1.R
@@ -0,0 +1,32 @@
+# We will use "sqldf" to read partial file.
+
+if(!require('sqldf')){
+  install.packages('sqldf')
+}
+library(sqldf)
+
+# Using read.csv.sql, we read the required data.
+
+data.file <- "./data/household_power_consumption.txt"
+sql <- "SELECT * FROM file WHERE Date='1/2/2007' OR Date='2/2/2007'"
+
+d <- read.csv.sql(data.file, sql, sep=";", header=TRUE)
+
+# Convert the data$Date & data$time into a datetime object.
+date.time <- paste(d$Date, d$Time, sep = " ")
+date.time <- strptime(date.time, format = "%d/%m/%Y %H:%M:%S")
+
+# Append date.time to the dataframe
+d$dateTime <- date.time
+
+# Drop the old Date & Time Column
+drops <- c("Date", "Time")
+d <- d[,!(names(d) %in% drops)]
+
+# Now create plot
+png(filename = "plot1.png", width = 480, height = 480, bg = "transparent")
+hist(d$Global_active_power,
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)",
+     col="red")
+dev.off()
